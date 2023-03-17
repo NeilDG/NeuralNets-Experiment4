34c609683d0e5117bbb7d38518d1e1782ab14308
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-17 22:38:56 +0800

    Bug fixes and optimization in training.

--- rtx3090_main.py
+++ rtx3090_main.py
@@ -6,8 +6,8 @@ def train_depth():
     #          "--iteration=10")
 
     os.system("python \"train_main.py\" --server_config=3 --img_to_load=-1 "
-              "--plot_enabled=1 --save_every_iter=100 --network_version=\"depth_v01.06\" "
-              "--iteration=13")
+              "--plot_enabled=1 --save_every_iter=100 --network_version=\"depth_v01.07\" "
+              "--iteration=9")
 
 def test_depth():
     os.system("python \"test_main.py\" --server_config=3 --img_to_load=-1 --plot_enabled=0 --network_version=\"depth_v01.03\" "
