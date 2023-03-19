fe18b2238949dda155908aa5b51bc5e75fb9f08b
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-19 21:32:15 +0800

    Slight bug fixes on GPU finding.

--- ccs1_main.py
+++ ccs1_main.py
@@ -4,12 +4,7 @@ import time
 
 import GPUtil
 
-def train_proper():
-    GPUtil.showUtilization()
-    device_id = GPUtil.getFirstAvailable(maxMemory=0.1, maxLoad=0.1, attempts=2500, interval=30, verbose=True)
-    gpu_device = "cuda:" + str(device_id[0])
-    print("Available GPU device found: ", gpu_device)
-
+def train_proper(gpu_device):
     os.system("python \"train_main.py\" --server_config=1 --img_to_load=-1 --cuda_device=" +gpu_device+ " "
              "--plot_enabled=0 --network_version=\"depth_v01.02\" "
              "--iteration=1")
@@ -48,6 +43,11 @@ def train_proper():
 def main():
     EXECUTION_TIME_IN_HOURS = 48
     execution_seconds = 3600 * EXECUTION_TIME_IN_HOURS
+
+    GPUtil.showUtilization()
+    device_id = GPUtil.getFirstAvailable(maxMemory=0.1, maxLoad=0.1, attempts=2500, interval=30, verbose=True)
+    gpu_device = "cuda:" + str(device_id[0])
+    print("Available GPU device found: ", gpu_device)
 
     p = multiprocessing.Process(target=train_proper, name="train_proper")
     p.start()
--- ccs2_main.py
+++ ccs2_main.py
@@ -4,12 +4,7 @@ import time
 
 import GPUtil
 
-def train_proper():
-    GPUtil.showUtilization()
-    device_id = GPUtil.getFirstAvailable(maxMemory=0.1, maxLoad=0.1, attempts=2500, interval=30, verbose=True)
-    gpu_device = "cuda:" + str(device_id[0])
-    print("Available GPU device found: ", gpu_device)
-
+def train_proper(gpu_device):
     os.system("python \"train_main.py\" --server_config=1 --img_to_load=-1 --cuda_device=" +gpu_device+ " "
              "--plot_enabled=0 --network_version=\"depth_v01.01\" "
              "--iteration=4")
@@ -17,7 +12,12 @@ def main():
     EXECUTION_TIME_IN_HOURS = 48
     execution_seconds = 3600 * EXECUTION_TIME_IN_HOURS
 
-    p = multiprocessing.Process(target=train_proper, name="train_proper")
+    GPUtil.showUtilization()
+    device_id = GPUtil.getFirstAvailable(maxMemory=0.1, maxLoad=0.1, attempts=2500, interval=30, verbose=True)
+    gpu_device = "cuda:" + str(device_id[0])
+    print("Available GPU device found: ", gpu_device)
+
+    p = multiprocessing.Process(target=train_proper, name="train_proper", args=(gpu_device,))
     p.start()
 
     time.sleep(10)  # causes p to execute code for X seconds. 3600 = 1 hour
