394b2d4b5976e639f5f54c0abf9d678d08af1a8a
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-23 12:12:05 +0800

    Updated gitignore for checkpoints.

--- rtx3090_main.py
+++ rtx3090_main.py
@@ -4,9 +4,9 @@ import time
 
 
 def train_depth():
-    os.system("python \"train_main.py\" --server_config=3 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.02\" "
-              "--iteration=10")
+    # os.system("python \"train_main.py\" --server_config=3 --img_to_load=-1 "
+    #           "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.02\" "
+    #           "--iteration=10")
 
     os.system("python \"train_main.py\" --server_config=3 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.02\" "
@@ -22,9 +22,9 @@ def test_depth():
               "--iteration=10")
 
 def main():
-    # train_depth()
-    test_depth()
-    # os.system("shutdown /s /t 1")
+    train_depth()
+    # test_depth()
+    os.system("shutdown /s /t 1")
 
 if __name__ == "__main__":
     main()
