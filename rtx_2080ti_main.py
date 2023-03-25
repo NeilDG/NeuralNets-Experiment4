0af4670eb5007011be0464364bd7280e9f53a684
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-25 12:14:11 +0800

    Training for RTX 2080Ti PC.

--- rtx_2080ti_main.py
+++ rtx_2080ti_main.py
@@ -1,46 +1,29 @@
 import os
 
 def train_depth():
-    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.10\" "
-              "--iteration=12")
-
-    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.10\" "
-              "--iteration=13")
-
-    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.10\" "
-              "--iteration=14")
-
-    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.10\" "
-              "--iteration=15")
-
-    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.10\" "
-              "--iteration=16")
-
-    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.10\" "
-              "--iteration=17")
-
-    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.10\" "
-              "--iteration=18")
-
-    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.10\" "
-              "--iteration=19")
+    os.system("python3 \"train_main.py\" --server_config=6 --img_to_load=-1 "
+              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.12\" "
+              "--iteration=1")
 
 def test_depth():
-    os.system("python \"test_main.py\" --server_config=2 --img_to_load=-1 --plot_enabled=1 --network_version=\"depth_v01.07\" "
-              "--iteration=10")
+    os.system("python3 \"test_main.py\" --server_config=6 --img_to_load=-1 --plot_enabled=1 --network_version=\"depth_v01.04\" "
+              "--iteration=12")
+
+def train_img2img():
+    os.system("python3 \"train_img2img_main.py\" --server_config=2 --img_to_load=-1 "
+              "--plot_enabled=0 --save_every_iter=200 --network_version=\"synth2real_v01.00\" "
+              "--iteration=3")
+
+def test_img2img():
+    os.system("python \"test_img2img_main.py\" --server_config=2 --img_to_load=1000 "
+              "--plot_enabled=1 --network_version=\"synth2real_v01.00\" "
+              "--iteration=1")
 
 def main():
-    train_depth()
+    # train_depth()
     # test_depth()
-    os.system("shutdown /s /t 1")
+    train_img2img()
+    # os.system("shutdown /s /t 1")
 
 if __name__ == "__main__":
     main()
