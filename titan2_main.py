004b9e13f765b00231bd04e8383bb37cb4820c49
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-26 20:52:11 +0800

    Bug fixes and optimization to batch sizes and CPU workers for RTX 3060

--- titan2_main.py
+++ titan2_main.py
@@ -13,16 +13,16 @@ def test_depth():
               "--iteration=12")
 
 def train_img2img():
-    os.system("python3 \"train_img2img_main.py\" --server_config=6 --img_to_load=-1 "
+    os.system("python3 \"train_img2img_main.py\" --server_config=5 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=200 --network_version=\"synth2real_v01.01\" "
               "--iteration=1")
 
-    os.system("python3 \"train_img2img_main.py\" --server_config=6 --img_to_load=-1 "
+    os.system("python3 \"train_img2img_main.py\" --server_config=5 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=200 --network_version=\"synth2real_v01.01\" "
               "--iteration=2")
 
 def test_img2img():
-    os.system("python \"test_img2img_main.py\" --server_config=6 --img_to_load=1000 "
+    os.system("python \"test_img2img_main.py\" --server_config=5 --img_to_load=1000 "
               "--plot_enabled=1 --network_version=\"synth2real_v01.00\" "
               "--iteration=1")
 
