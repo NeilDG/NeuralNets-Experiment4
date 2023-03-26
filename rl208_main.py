23e6ac2d0db6d687ffaf6c0c89dec7c2c34ab03d
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-26 21:02:17 +0800

    Bug fixes in early stopper. Updated RL208 GPU script.

--- rl208_main.py
+++ rl208_main.py
@@ -6,7 +6,7 @@ import time
 def train_depth():
     os.system("python \"train_main.py\" --server_config=4 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.12\" "
-              "--iteration=1")
+              "--iteration=2")
 
 def test_depth():
     os.system("python \"test_main.py\" --server_config=4 --img_to_load=-1 --plot_enabled=1 --network_version=\"depth_v01.04\" "
