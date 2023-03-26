89f15b618f730e5a9413c56724f9432ad624751e
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-26 21:08:07 +0800

    Update rl208_main.py
    Former-commit-id: ec3ae7fb3f72756d1ffdbbe3f14072d5c3866fee [formerly 9c00c655240c9129d26ba88ed85d287b397186c6]
    Former-commit-id: ed4b1eff94939282ead2eab1128e3feb78b800bd

--- rl208_main.py
+++ rl208_main.py
@@ -4,7 +4,11 @@ import time
 
 
 def train_depth():
-    os.system("python \"train_main.py\" --server_config=4 --img_to_load=-1 "
+    os.system("python \"train_main.py\" --server_config=3 --img_to_load=-1 "
+              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.12\" "
+              "--iteration=1")
+    
+    os.system("python \"train_main.py\" --server_config=3 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.12\" "
               "--iteration=2")
 
