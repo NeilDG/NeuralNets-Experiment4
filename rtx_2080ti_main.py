a9e3bf4668049a3b1995309da362f32d2c11dfb0
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-29 22:13:24 +0800

    Update rtx_2080ti_main.py
    Former-commit-id: 773e57ec92d274bb53ba3fd142c21504e33c2fec [formerly 15bd71f7d7cf2e406469170ec9204179bcd0860d]
    Former-commit-id: 8403096aa997655ca04b17579f2991571c1e094b

--- rtx_2080ti_main.py
+++ rtx_2080ti_main.py
@@ -1,24 +1,24 @@
 import os
 
 def train_depth():
-    os.system("python3 \"train_main.py\" --server_config=2 --img_to_load=-1 "
+    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.14\" "
               "--iteration=5")
 
-    os.system("python3 \"train_main.py\" --server_config=2 --img_to_load=-1 "
+    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.14\" "
               "--iteration=6")
 
-    os.system("python3 \"train_main.py\" --server_config=2 --img_to_load=-1 "
+    os.system("python \"train_main.py\" --server_config=2 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.14\" "
               "--iteration=7")
 
 def test_depth():
-    os.system("python3 \"test_main.py\" --server_config=2 --img_to_load=-1 --plot_enabled=1 --network_version=\"depth_v01.04\" "
+    os.system("python \"test_main.py\" --server_config=2 --img_to_load=-1 --plot_enabled=1 --network_version=\"depth_v01.04\" "
               "--iteration=12")
 
 def train_img2img():
-    os.system("python3 \"train_img2img_main.py\" --server_config=2 --img_to_load=-1 "
+    os.system("python \"train_img2img_main.py\" --server_config=2 --img_to_load=-1 "
               "--plot_enabled=0 --save_every_iter=200 --network_version=\"synth2real_v01.00\" "
               "--iteration=3")
 
@@ -31,7 +31,7 @@ def main():
     train_depth()
     # test_depth()
     # train_img2img()
-    # os.system("shutdown /s /t 1")
+    os.system("shutdown /s /t 1")
 
 if __name__ == "__main__":
     main()
