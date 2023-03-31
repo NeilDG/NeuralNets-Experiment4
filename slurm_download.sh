1e50feb587c9e15e04ba7fb3ce9e4b80204fe36c
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-03-31 10:35:05 +0800

    Updated slurm download for COARE.
    Former-commit-id: fac2c46f4333519c0cf4c3a603a287e11ab640ca [formerly 72f6fa80f22154d8be7ccee30fdd757535db9d28]
    Former-commit-id: 1159816a3c64cf3ec0ece6e5459d9fea7aa2bd1e

--- slurm_download.sh
+++ slurm_download.sh
@@ -17,18 +17,18 @@ module load cuda/10.1_cudnn-7.6.5
 source activate NeilGAN_V2
 
 #do fresh install
-pip-review --local --auto
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install scikit-learn
-pip install scikit-image
-pip install visdom
-pip install kornia
-pip install opencv-python==4.5.5.62
-pip install --upgrade pillow
-pip install gputil
-pip install matplotlib
-pip install --upgrade --no-cache-dir gdown
-pip install PyYAML
+#pip-review --local --auto
+#pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+#pip install scikit-learn
+#pip install scikit-image
+#pip install visdom
+#pip install kornia
+#pip install opencv-python==4.5.5.62
+#pip install --upgrade pillow
+#pip install gputil
+#pip install matplotlib
+#pip install --upgrade --no-cache-dir gdown
+#pip install PyYAML
 
 if [ $SERVER_CONFIG == 0 ]
 then
